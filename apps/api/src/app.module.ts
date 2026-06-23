03a8721f81c2710c00aa951a822e28eca737725d
unknown <ldat0909@gmail.com>
2026-06-23 13:59:32 +0700

    feat(infra): add PrismaService and run initial database migration

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './infrastructure/persistence/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [],
   providers: [],
 })
